5bea67ca672c7f32beecaf21c7c0313fd756d34c
Ram Kumar Hariharan <ramk@gatech.edu>
2010-10-05 15:22:43 -0400

    BaselineFeatureExtractor working

--- src/LinguisticFeatureExtractor.java
+++ src/LinguisticFeatureExtractor.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
@@ -8,6 +10,7 @@ import weka.core.Attribute;
 import weka.core.FastVector;
 import weka.core.Instance;
 import weka.core.Instances;
+import weka.core.converters.ArffSaver;
 import edu.stanford.nlp.parser.lexparser.LexicalizedParser;
 import edu.stanford.nlp.trees.GrammaticalStructure;
 import edu.stanford.nlp.trees.GrammaticalStructureFactory;
@@ -19,7 +22,8 @@ import edu.stanford.nlp.trees.TypedDependency;
 public class LinguisticFeatureExtractor implements FeatureExtractor{
 	private FastVector attrs;
 	private FastVector pos_relations;
-	
+	private static final int OFFSET = 1;
+
 	private void setupAttributes(List<Tweet> tweets)
 	{
 		attrs = new FastVector();
@@ -28,19 +32,25 @@ public class LinguisticFeatureExtractor implements FeatureExtractor{
 		sentvals.addElement("Negative"); sentvals.addElement("Neutral"); sentvals.addElement("Positive"); 
 		Attribute sentclass = new Attribute("Sentiment", sentvals);
 		attrs.addElement(sentclass);
-		
-		pos_relations = get_all_pos_relations(tweets, attrs.size());
-		Attribute pos_attr = new Attribute("POSRelations", pos_relations);
-		attrs.addElement(pos_attr);
+
+		pos_relations = get_all_pos_relations(tweets);
+		for(int i=0; i<pos_relations.size(); i++)
+		{
+			String word = (String) pos_relations.elementAt(i);
+			Attribute attr = new Attribute(word);
+			attrs.addElement(attr);
+		}
 	}
-	
+
 	@Override
 	public Instances extractFeatures(List<Tweet> tweets) {
-		// TODO Auto-generated method stub	
+		if(attrs == null)
+			setupAttributes(tweets);
+		
 		Instances feats = new Instances("Linguistic Features", attrs, tweets.size());
 		feats.setClassIndex(0);
 		// Record features
-		
+
 		StringTokenizer st;
 		String word;
 		List words;
@@ -50,41 +60,51 @@ public class LinguisticFeatureExtractor implements FeatureExtractor{
 		GrammaticalStructure gs;
 		Collection tdl;
 		LexicalizedParser lp = new LexicalizedParser("res/englishPCFG.ser.gz");
-	    lp.setOptionFlags(new String[]{"-maxLength", "80", "-retainTmpSubcategories"});
-	    tlp = new PennTreebankLanguagePack();
+		lp.setOptionFlags(new String[]{"-maxLength", "80", "-retainTmpSubcategories"});
+		tlp = new PennTreebankLanguagePack();
 		gsf = tlp.grammaticalStructureFactory();
-		
+
 		for(Tweet t: tweets)
 		{	
-			Instance inst = new Instance(attrs.size());
+			Instance inst = new Instance(1.0, new double[attrs.size()]);
 			inst.setClassValue(t.sentiment);
 
-		    st = new StringTokenizer(t.text);
-		    words = new ArrayList<String>();
+			st = new StringTokenizer(t.text);
+			words = new ArrayList<String>();
 			while(st.hasMoreElements()) {
 				word = (String) st.nextElement();
 				words.add(word);
 			}
-		    parse = (Tree) lp.apply(words);
+			parse = (Tree) lp.apply(words);
 			gs = gsf.newGrammaticalStructure(parse);
 			tdl = gs.typedDependenciesCollapsed();
-			
+
 			Iterator td_itr = tdl.iterator();
 			TypedDependency x;
 			while(td_itr.hasNext()) {
 				x = (TypedDependency) td_itr.next();
 				if(pos_relations.contains(x.reln().toString()))
-					inst.setValueSparse(pos_relations.indexOf(x.reln().toString()), 1);
+					inst.setValue(pos_relations.indexOf(x.reln().toString())+OFFSET, 1);
 			}
-			
+
 			inst.setDataset(feats);
 			feats.add(inst);
 		}
-		
+
+		ArffSaver saver = new ArffSaver();
+		saver.setInstances(feats);
+		try {
+			saver.setFile(new File("output/baseline.arff"));
+			saver.writeBatch();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		return feats;
 	}
-	
-	private static FastVector get_all_pos_relations(List<Tweet> tweets, int attr_size)
+
+	private static FastVector get_all_pos_relations(List<Tweet> tweets)
 	{
 		StringTokenizer st;
 		String word;
@@ -95,32 +115,35 @@ public class LinguisticFeatureExtractor implements FeatureExtractor{
 		GrammaticalStructure gs;
 		Collection tdl;
 		LexicalizedParser lp = new LexicalizedParser("res/englishPCFG.ser.gz");
-	    lp.setOptionFlags(new String[]{"-maxLength", "80", "-retainTmpSubcategories"});
-	    tlp = new PennTreebankLanguagePack();
+		lp.setOptionFlags(new String[]{"-maxLength", "80", "-retainTmpSubcategories"});
+		tlp = new PennTreebankLanguagePack();
 		gsf = tlp.grammaticalStructureFactory();
+		int postags_count = 0;
 		FastVector pos_relations = new FastVector();
-		
+
 		for(Tweet t: tweets)
 		{		
-			Instance inst = new Instance(attr_size);
-			inst.setClassValue(t.sentiment);
-
-		    st = new StringTokenizer(t.text);
-		    words = new ArrayList<String>();
+			st = new StringTokenizer(t.text);
+			words = new ArrayList<String>();
 			while(st.hasMoreElements()) {
 				word = (String) st.nextElement();
 				words.add(word);
 			}
-		    parse = (Tree) lp.apply(words);
+			parse = (Tree) lp.apply(words);
 			gs = gsf.newGrammaticalStructure(parse);
 			tdl = gs.typedDependenciesCollapsed();
-			
+
 			Iterator td_itr = tdl.iterator();
 			TypedDependency x;
 			while(td_itr.hasNext()) {
 				x = (TypedDependency) td_itr.next();
-				pos_relations.addElement(x.toString());
+				if(!pos_relations.contains(x.reln().toString())) {
+					pos_relations.addElement(x.reln().toString());
+					postags_count++;
+				}
 			}
+			if(postags_count > 500)
+				break;
 		}		
 		return pos_relations;
 	}
--- src/SVMClassification.java
+++ src/SVMClassification.java
@@ -4,47 +4,64 @@ import weka.classifiers.functions.SMO;
 import weka.core.Attribute;
 import weka.core.Instances;
 
-
 public class SVMClassification {
 	private FeatureExtractor featexts[] = new FeatureExtractor[4];
 	private SMO classifiers[] = new SMO[4];
 	private Instances insts[] = new Instances[4];
-	
+
 	public void train(String fileloc) {
 		ArrayList<Tweet> tweets = TweetFileParser.parseFile(fileloc);
-		//featexts[0] = new BaselineFeatureExtractor();
 		featexts[2] = new ContextualFeatureExtractor();
-		
-		
+		//featexts[2] = new BaselineFeatureExtractor();
+		//featexts[2] = new LinguisticFeatureExtractor();
+
 		insts[2] = featexts[2].extractFeatures(tweets);
 		classifiers[2] = new SMO();
-		String[] opts = {"-C 1.0", "-L 0.0010", "-P 1.0E-12", "-N 0", "-V -1", "-W 1", "-K \"weka.classifiers.functions.supportVector.PolyKernel -C 250007 -E 1.0\""};
+		// String[] opts = {"-C 1.0", "-L 0.0010", "-P 1.0E-12", "-N 0",
+		// "-V -1", "-W 1",
+		// "-K \"weka.classifiers.functions.supportVector.PolyKernel -C 250007 -E 1.0\""};
+		//String[] opts = { "-C 1.0", "-L 0.0010", "-P 1.0E-12", "-N 0", "-V -1",
+		//		"-W 1"};
+		//		"-K \"weka.classifiers.functions.supportVector.PolyKernel -C 250007 -E 1.0\"" };
 		try {
-			classifiers[2].setOptions(opts);
+			//classifiers[2].setOptions(opts);
 			classifiers[2].buildClassifier(insts[2]);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
-	public void test(String fileloc)
-	{
+
+	public void test(String fileloc) {
 		ArrayList<Tweet> tweets = TweetFileParser.parseFile(fileloc);
 		Instances tests = featexts[2].extractFeatures(tweets);
-		
-		for(int i=0;i<tests.numInstances();i++) {
+		int accuracy = 0;
+		int nonneutral = 0;
+		for (int i = 0; i < tests.numInstances(); i++) {
 			try {
 				double cld = classifiers[2].classifyInstance(tests.instance(i));
 				Attribute attr = tests.attribute(0);
 				String cl = attr.value((int) cld);
-				System.out.println(cl);
+				double actd = tests.instance(i).value(0);
+				String act = attr.value((int) actd);
+
+				if(!act.equals("Neutral")) {
+					nonneutral++;
+					System.out.println("Class: " + cl + ", Act: " + act);
+					if (cl.equals(act))
+						accuracy++;
+				}
+
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}
-			
+			}	
 		}
+		System.out.println(tests.numInstances());
+		System.out.println(nonneutral);
+		System.out.println(accuracy);
+		System.out.println("Accuracy:");
+		System.out.println(accuracy / (double)nonneutral);
 	}
 
 	/**
