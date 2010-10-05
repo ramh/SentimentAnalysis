430b75cef695c5588321742c4724e9d98ee439b3
Kelsey Hawkins <kphawkins@gmail.com>
2010-10-05 16:28:12 -0400

    Wrote final code for SVMClassification.java but commented out for testing.  Added python file for parsing ASSERT output

---
+++ convert_parses_2.py
@@ -0,0 +1,88 @@
+import sys
+import string
+import re
+
+tag_trans = { "ARG0": "WHO", "TARGET": "WHAT",
+              "ARGM-TMP": "WHEN", "ARGM-LOC": "WHERE",
+              "ARGM-PRP": "WHY1",
+              "ARGM-PNC": "WHY2", "ARGM-CAU": "WHY3"}
+    
+#---- this is the file from which the sentences are to be extracted ----#
+lines = open(sys.argv[1]).readlines()
+
+matcher = re.compile(r"\[(?P<tag>[^ ]*) (?P<chunk>[^\]]*)\]")
+
+nums, nlines, ntagslist, ntagsnums = [], [], [], []
+uniqtags, tagsdicts = [], [] #mod
+
+for line in lines:
+    stind, endind = 0, len(line)
+    ntagsnum = 0
+    begs, tags, chunks = [], [], []
+    tagsdict = {} #mod
+    nline = ""
+    num, parse = line.split(':')
+    nums += [int(num)]
+    parse = parse.strip()
+    cur = parse
+    match = matcher.search(cur,stind,endind)
+    while match != None:
+        beg, tag, chunk, end = cur[0:match.span('tag')[0]-1], cur[match.span('tag')[0]:match.span('tag')[1]], cur[match.span('chunk')[0]:match.span('chunk')[1]], cur[match.span('chunk')[1]+1:endind]
+        begs += [beg]
+        tags += [tag]
+        
+        # modified code for assign 2
+        if tag in tagsdict:
+            tagsdict[tag] += 1
+        else:
+            tagsdict += {tag : 1}
+        if not tag in uniqtags:
+            uniqtags += [tag]
+        # end mod code
+        
+        chunks += [chunk]
+        cur = end
+        match = matcher.search(cur,0,len(cur))
+    ntags = []
+    for tag in tags:
+        if tag in tag_trans:
+            ntags += [tag_trans[tag]]
+        else:
+            ntags += [tag]
+        ntagsnum += 1
+    for i in range(len(begs)):
+        if ntags[i] != None:
+            nline += begs[i] + "[%s %s]" % (ntags[i].strip(), chunks[i].strip())
+        else:
+            nline += begs[i] + chunks[i]
+    nline += end
+    nlines += [nline]
+    ntagslist += [ntags]
+    ntagsnums += [ntagsnum]
+    tagsdicts += [tagsdict] #mod
+
+curlinenum = 0
+
+print ",".join(uniqtags)
+    
+i = 0
+while i < len(nums):
+#    import pdb; pdb.set_trace()
+    while nums[i] != curlinenum:
+        #print "%d: (no parse)" % (curlinenum + 1) 
+        print ""
+        curlinenum += 1
+    if nums[i] != nums[i+1]:
+        prline = nlines[i]
+    else:
+        j = i
+        while j < len(nums) and nums[j] == nums[i]:
+            j += 1
+        cnums = ntagsnums[i:j]
+        useind = cnums.index(max(cnums)) + i
+        prline = nlines[useind]
+        i = j - 1
+    #print "%d: %s" % (curlinenum + 1, prline) 
+    print ",".join(["%s:%d" % (tag, count) for (tag, count) in tagsdicts[useind]])
+    curlinenum += 1
+    i += 1
--- src/SVMClassification.java
+++ src/SVMClassification.java
@@ -6,12 +6,32 @@ import weka.core.Instances;
 
 
 public class SVMClassification {
-	private FeatureExtractor featexts[] = new FeatureExtractor[4];
-	private SMO classifiers[] = new SMO[4];
-	private Instances insts[] = new Instances[4];
+	private FeatureExtractor featexts[] = {new BaselineFeatureExtractor(),
+										   new ContextualFeatureExtractor()};
+	private SMO classifiers[] = new SMO[featexts.length];
+	private Instances insts[] = new Instances[featexts.length];
 	
 	public void train(String fileloc) {
 		ArrayList<Tweet> tweets = TweetFileParser.parseFile(fileloc);
+		
+		/* Full actual code
+		for(int i=0;i<featexts.length;i++) {
+			insts[i] = featexts[i].extractFeatures(tweets);
+			if(i > 0)
+				insts[i] = Instances.mergeInstances(insts[i], insts[i-1]);
+			classifiers[i] = new SMO();
+			String[] opts = {"-C 1.0", "-L 0.0010", "-P 1.0E-12", "-N 0", "-V -1", "-W 1", "-K \"weka.classifiers.functions.supportVector.PolyKernel -C 250007 -E 1.0\""};
+			try {
+				classifiers[i].setOptions(opts);
+				classifiers[i].buildClassifier(insts[i]);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}			
+		}
+		*/
+		
+		// Testing code:
 		//featexts[0] = new BaselineFeatureExtractor();
 		featexts[2] = new ContextualFeatureExtractor();
 		
@@ -31,6 +51,27 @@ public class SVMClassification {
 	public void test(String fileloc)
 	{
 		ArrayList<Tweet> tweets = TweetFileParser.parseFile(fileloc);
+		
+		/* Full actual code
+		for(int i=0;i<featexts.length;i++) {
+			insts[i] = featexts[i].extractFeatures(tweets);
+			if(i > 0)
+				insts[i] = Instances.mergeInstances(insts[i], insts[i-1]);
+			for(int j=0;j<tests.numInstances();j++) {
+				try {
+					double cld = classifiers[i].classifyInstance(tests.instance(j));
+					Attribute attr = tests.attribute(0);
+					String cl = attr.value((int) cld);
+					System.out.println(cl);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+		}
+		*/
+		
 		Instances tests = featexts[2].extractFeatures(tweets);
 		
 		for(int i=0;i<tests.numInstances();i++) {
